7d32cebb55af1a6a43cc0155e613da158d238399
abdoulkarim20 <abdoulkarimfoly1996@gmail.com>
2022-10-04 11:51:56 +1300

    using useEffet for secondary changement or effect to component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [name, setName] = useState("Abdoul Karim DIALLO");
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("#ffffff");
+  useEffect(() => {
+    setColor(count < 0 ? "#ce0033" : "#ffffff")
+    console.log('le changement est', count);
+  }, [count])
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Salut {name}
-        </p>
-        <input onChange={(e)=>{setName(e.target.value)}}></input>
+        <div>
+          <p>
+            Salut {name}
+          </p>
+          <input onChange={(e) => { setName(e.target.value) }}></input>
+        </div>
+        <div style={{ color:color  }}>
+          <p>Vous avez cliquer {count} fois</p>
+          <button onClick={() => { setCount(count - 1) }}>-</button>
+          <button onClick={() => { setCount(count + 1) }}>+</button>
+        </div>
       </header>
     </div>
   );
