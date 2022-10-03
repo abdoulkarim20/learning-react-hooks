5784d01e8626b83c0e7ff2ec158278278ca48885
abdoulkarim20 <abdoulkarimfoly1996@gmail.com>
2022-10-04 12:45:04 +1300

    using useRef for applicate the programming imperative and manupilating the dom element

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 
 function App() {
+  const inpupRef=useRef();
   const [name, setName] = useState("Abdoul Karim DIALLO");
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState(null);
+  const resetCount = () => { setCount(0) }
   const [color, setColor] = useState("#ffffff");
+  const handleOnchange = (e) => { setValue(e.target.value) };
+  const onIncrement = () => {
+    inpupRef.current.value=null;
+    setValue(null);
+    !!value && setCount(count + parseInt(value))
+  }
+  const onDecrement = () => {
+    inpupRef.current.value=null;
+    setValue(null);
+    /*controle de flux*/
+    !!value && setCount(count - parseInt(value))
+  }
   useEffect(() => {
     setColor(count < 0 ? "#ce0033" : "#ffffff")
-    console.log('le changement est', count);
+    // console.log('le changement est', count);
   }, [count])
   return (
     <div className="App">
@@ -18,10 +33,18 @@ function App() {
           </p>
           <input onChange={(e) => { setName(e.target.value) }}></input>
         </div>
-        <div style={{ color:color  }}>
-          <p>Vous avez cliquer {count} fois</p>
-          <button onClick={() => { setCount(count - 1) }}>-</button>
-          <button onClick={() => { setCount(count + 1) }}>+</button>
+        <div style={{ color: color }}>
+          {count}
+          <div>
+            <input ref={inpupRef} type="number" onChange={handleOnchange}></input>
+          </div>
+          <div>
+            <button onClick={resetCount}>reset count</button>
+          </div>
+          <div>
+            <button onClick={onDecrement}>-</button>
+            <button onClick={onIncrement}>+</button>
+          </div>
         </div>
       </header>
     </div>
